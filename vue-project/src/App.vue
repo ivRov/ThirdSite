190db1d5fdbcad361906ed116ac4ae765a81aa79
Your Name <you@example.com>
2023-05-10 19:03:47 +0400

    first commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<div><nav-bar/>
+<router-view></router-view>
+</div>
+</template>
+
+<script>
+
+import navBar from './components/navBar.vue'
+export default {
+components: {
+  navBar,
+
+}
+}
+</script>
+
+<style>
+</style>
---
+++ vue-project/src/components/aboutUs.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="logoFilmCoub">
+  <img src="../assets/img/img1AboutUs.png">
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.logoFilmCoub {
+  text-align: center;
+}
+
+</style>
